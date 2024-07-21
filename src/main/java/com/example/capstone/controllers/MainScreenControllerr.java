1d1ac307166b42199c287113fbc568f3efda988d
Cheyenne Allen <cheyenneallen@MacBook-Air.local>
2024-07-21 03:44:23 -0400

    Add custom css

--- src/main/java/com/example/capstone/controllers/MainScreenControllerr.java
+++ src/main/java/com/example/capstone/controllers/MainScreenControllerr.java
@@ -2,9 +2,12 @@ package com.example.capstone.controllers;
 
 import com.example.capstone.domain.Part;
 import com.example.capstone.domain.Product;
+import com.example.capstone.service.CustomUserDetailsService;
 import com.example.capstone.service.PartService;
 import com.example.capstone.service.ProductService;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,10 +25,14 @@ import java.util.List;
 public class MainScreenControllerr {
     private PartService partService;
     private ProductService productService;
+    private CustomUserDetailsService customUserDetailsService;
 
-    public MainScreenControllerr(PartService partService,ProductService productService){
+    public MainScreenControllerr(PartService partService,
+                                 ProductService productService,
+                                 CustomUserDetailsService customUserDetailsService){
         this.partService=partService;
         this.productService=productService;
+        this.customUserDetailsService=customUserDetailsService;
     }
     @GetMapping("/mainscreen")
     public String listPartsandProducts(Model theModel, @Param("partkeyword") String partkeyword, @Param("productkeyword") String productkeyword){
@@ -37,6 +44,13 @@ public class MainScreenControllerr {
         List<Product> productList=productService.listAll(productkeyword);
         theModel.addAttribute("products", productList);
         theModel.addAttribute("productkeyword",productkeyword);
+
+        // display current user
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        theModel.addAttribute("username", currentUsername);
+
         return "mainscreen";
     }
+
 }
