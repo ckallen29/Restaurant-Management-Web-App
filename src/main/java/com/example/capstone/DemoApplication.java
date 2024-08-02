21e9025d643ca6f93a67a39a9bf080968a28e9f9
Cheyenne Allen <cheyenneallen@MacBook-Air.local>
2024-08-02 17:03:15 -0400

    Hide RDS credentials

--- src/main/java/com/example/capstone/DemoApplication.java
+++ src/main/java/com/example/capstone/DemoApplication.java
@@ -1,13 +1,33 @@
 package com.example.capstone;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        try (InputStream inputStream = DemoApplication.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (inputStream == null) {
+                System.out.println("Unable to find config.properties");
+                return;
+            }
+            //load properties file
+            properties.load(inputStream);
+            //set properties as system properties
+            System.setProperty("DB_USERNAME", properties.getProperty("DB_USERNAME"));
+            System.setProperty("DB_PASSWORD", properties.getProperty("DB_PASSWORD"));
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
 }
